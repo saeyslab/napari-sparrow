a55d2ea9b5f4d4420cd52a429fabcab57d1363f5
Frank Vernaillen <frank.vernaillen@vib.be>
2022-07-01 00:30:46 +0200

    Move segmentation to a worker thread.
    This keeps the napari UI responsive during the long running segmentation
    process. Additionally delay importing the cellpose package until it is really
    needed so the segmentation widget appears immediately when the user
    opens it.

--- src/napari_spongepy/_segmentation_widget.py
+++ src/napari_spongepy/_segmentation_widget.py
@@ -11,32 +11,47 @@ import napari.layers
 import napari.types
 import numpy as np
 from typing import List
-import torch
-from cellpose import models
+from napari.qt.threading import thread_worker
+
+
+@thread_worker
+def _segmentation_worker(img: np.ndarray,
+                         use_gpu: bool,
+                         min_size: int,
+                         flow_threshold: float,
+                         diameter: int,
+                         mask_threshold: int) -> np.ndarray:
+
+    # We delay importing the large cellpose module to when it is really needed over here.
+    # This makes the segmentation widget appear almost instantaneously when it is opened in napari,
+    # otherwise it would only appear after the cellpose package is imported, which takes several seconds.
+    from cellpose import models
+
+    model = models.Cellpose(gpu=use_gpu, model_type='nuclei')
+    channels = np.array([0, 0])
+
+    masks, _, _, _ = model.eval(img,
+                                diameter=diameter,
+                                channels=channels,
+                                min_size=min_size,
+                                flow_threshold=flow_threshold,
+                                cellprob_threshold=mask_threshold)
+    return masks
 
 
 @magic_factory(call_button='Segment')
-def segmentation_widget(image: napari.layers.Image,
+def segmentation_widget(viewer: napari.Viewer,
+                        image: napari.layers.Image,
                         use_gpu: bool=True,
                         min_size: int=80,
                         flow_threshold: float=0.6,
                         diameter: int=55,
-                        mask_threshold: int=0) -> List[napari.types.LayerDataTuple]:
+                        mask_threshold: int=0):
 
     print(f'About to segment {image} using Cellpose; use_gpu={use_gpu}')
     if image is None:
-        return []
+        return
 
-    img = image.data
-
-    model = models.Cellpose(gpu=use_gpu, model_type='nuclei')
-    channels = np.array([0,0])
-    
-    masks, flows, styles, diams = model.eval(img, 
-                                             diameter=diameter,
-                                             channels=channels,
-                                             min_size=min_size,
-                                             flow_threshold=flow_threshold,
-                                             cellprob_threshold=mask_threshold)
-
-    return [(masks, {'name': 'Segmentation'}, 'labels')]
+    worker = _segmentation_worker(image.data, use_gpu, min_size, flow_threshold, diameter, mask_threshold)
+    worker.returned.connect(lambda label_img: viewer.add_labels(label_img, name='Segmentation'))
+    worker.start()
