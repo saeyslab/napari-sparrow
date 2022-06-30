4ec5821b9e1f38b618aa3e8e6ed0a2e6aa61bf55
Frank Vernaillen <frank.vernaillen@vib.be>
2022-07-01 00:49:04 +0200

    Cosmetic changes

--- src/napari_spongepy/_segmentation_widget.py
+++ src/napari_spongepy/_segmentation_widget.py
@@ -1,16 +1,15 @@
 """
-Napari widget for cell segmentation of 
+Napari widget for cell segmentation of
 preprocessed (Resolve) spatial transcriptomics
 microscopy images with nuclear stains.
 Segmentation is performed with Cellpose.
 """
 
-from magicgui import magic_factory
 import napari
 import napari.layers
 import napari.types
 import numpy as np
-from typing import List
+from magicgui import magic_factory
 from napari.qt.threading import thread_worker
 
 
@@ -22,9 +21,11 @@ def _segmentation_worker(img: np.ndarray,
                          diameter: int,
                          mask_threshold: int) -> np.ndarray:
 
-    # We delay importing the large cellpose module to when it is really needed over here.
-    # This makes the segmentation widget appear almost instantaneously when it is opened in napari,
-    # otherwise it would only appear after the cellpose package is imported, which takes several seconds.
+    # We delay importing the large cellpose module to when it is really
+    # needed over here. This makes the segmentation widget appear almost
+    # instantaneously when it is opened in napari, otherwise it would only
+    # appear after the cellpose package is imported, which takes several
+    # seconds.
     from cellpose import models
 
     model = models.Cellpose(gpu=use_gpu, model_type='nuclei')
@@ -42,11 +43,11 @@ def _segmentation_worker(img: np.ndarray,
 @magic_factory(call_button='Segment')
 def segmentation_widget(viewer: napari.Viewer,
                         image: napari.layers.Image,
-                        use_gpu: bool=True,
-                        min_size: int=80,
-                        flow_threshold: float=0.6,
-                        diameter: int=55,
-                        mask_threshold: int=0):
+                        use_gpu: bool = True,
+                        min_size: int = 80,
+                        flow_threshold: float = 0.6,
+                        diameter: int = 55,
+                        mask_threshold: int = 0):
 
     print(f'About to segment {image} using Cellpose; use_gpu={use_gpu}')
     if image is None:
