a7497b1e989a4060f37f6d71f1edf4b9ef546d09
Frank Vernaillen <frank.vernaillen@vib.be>
2022-07-06 12:54:18 +0200

    Refactor transcript visualization widget.
    Cleaned up the code a bit. Next steps are moving the calculations
    off the napari GUI thread, and
    perhaps to use Dask to speed up density
    map calculation.

--- src/napari_spongepy/_transcripts_widget.py
+++ src/napari_spongepy/_transcripts_widget.py
@@ -1,6 +1,5 @@
 import math
 import pathlib
-from typing import List, Optional
 
 import napari
 import napari.layers
@@ -43,88 +42,113 @@ COLOR_CYCLE2 = (
     call_button="Visualize", density_radius_micrometer={"min": 0.1, "max": 10.0}
 )
 def transcripts_widget(
+    viewer: napari.Viewer,
     image: napari.layers.Image,
     transcripts_file: pathlib.Path = pathlib.Path(""),
     genes_to_plot: str = "",  # comma-separated list of genes to plot, e.g. 'Acta2,Xcr1'; gene names are case sensitive
-    density_plot: bool = True,
+    plot_density: bool = True,
     density_radius_micrometer: float = 3.45,  # 3.45 is Polylux default
+    plot_points: bool = True,
     point_size: int = 10,
-) -> List[napari.types.LayerDataTuple]:
+):
     print("About to Visualize transcripts")
 
-    df = pd.read_csv(transcripts_file, delimiter="\t", header=None)
-    # df has a row for each transcript detected in the Resolve experiment;
+    # Read CSV file with transcript coordinates and name.
+    # The resulting dataframe df has a row for each transcript detected in the Resolve experiment;
     # each row is "x y z gene" with the (x,y,z) coordinate of the position where the transcript for 'gene' was detected.
+    df = pd.read_csv(
+        transcripts_file,
+        delimiter="\t",
+        header=None,
+        usecols=[0, 1, 2, 3],
+        names=["x", "y", "z", "gene"],
+    )
 
-    gene_codes, unique_genes = pd.factorize(df.iloc[:, 3], sort=True)
-    df["gene_codes"] = gene_codes
-    print(gene_codes)
-    print(unique_genes)
+    # Assign a unique integer value with each unique gene.
+    # This is useful for coloring identical genes with the the same color lateron.
+    gene_codes, unique_genes = pd.factorize(df["gene"], sort=True)
+    df["gene_index"] = gene_codes
 
-    print(f"Got {len(gene_codes)} transcripts for {len(unique_genes)} unique genes")
+    print(
+        f"Got {len(gene_codes)} transcripts for these {len(unique_genes)} unique genes: {list(unique_genes)}"
+    )
 
-    # Specify a selection of genes to display
-    selected_genes = genes_to_plot.replace(" ", "").split(
-        ","
-    )  # remove whitespace and split, e.g. 'Acta2, Xcr1' -> ['Acta2', 'Xcr1']
+    # Make a list with the genes to display.
+    # Remove whitespace and split comma-separated string with gene names, e.g. 'Acta2, Xcr1' -> ['Acta2', 'Xcr1']
+    selected_genes = genes_to_plot.replace(" ", "").split(",")
     print(f"Selected genes={selected_genes}")
 
     # Slice the transcripts dataframe to only hold transcripts for the selected genes.
-    selected_df = df[df[3].isin(selected_genes)]
+    selected_df = df[df["gene"].isin(selected_genes)]
 
-    coords = selected_df.iloc[
-        :, [1, 0]
-    ]  # change the order from x,y to y,x for use in napari
-    gene_names = list(selected_df.iloc[:, 3])  # column 3 is the gene name
-    gene_index = list(selected_df["gene_codes"])
+    if selected_df.empty:
+        print(
+            "No transcripts for the specified list of genes. Is the list empty, or does it have typos?"
+        )
+        return
+
+    if plot_density:
+        layer = _make_transcript_density_layer(
+            image, selected_df, density_radius_micrometer
+        )
+        viewer.add_image(
+            layer[0], **layer[1]
+        )  # FIXME: we want to layer to be updated if it already exists
+
+    if plot_points:
+        layer = _make_transcript_points_layer(selected_df, point_size)
+        viewer.add_points(
+            layer[0], **layer[1]
+        )  # FIXME: we want to layer to be updated if it already exists
+
+
+def _make_transcript_density_layer(
+    image: napari.layers.Image, df: pd.DataFrame, density_radius_micrometer: float
+) -> napari.types.LayerDataTuple:
+
+    print("Calculating density map")
+    density_radius_pixels = math.ceil(density_radius_micrometer / RESOLVE_PIXEL_SIZE)
+    spots = df[["y", "x"]].to_numpy()
+    density = _calculate_density_map(image.data.shape, spots, density_radius_pixels)
+
+    max_density = np.max(density)
+    if max_density > 0:
+        density_image = density / np.max(density)
+    else:
+        density_image = np.zeros(image.data.shape, dtype=np.uint8)
+
+    layer = (
+        density_image,
+        {"name": "transcript density", "colormap": "inferno", "opacity": 0.75},
+    )
+    return layer
+
+
+def _make_transcript_points_layer(
+    df: pd.DataFrame, point_size: int
+) -> napari.types.LayerDataTuple:
 
     point_properties = {
-        "gene": gene_names,  # shown when user hovers over a point in the GUI
-        "gene_index": gene_index,  # used for coloring
+        "gene": list(
+            df["gene"]
+        ),  # gene name, shown when user hovers over a point in the GUI
+        "gene_index": list(
+            df["gene_index"]
+        ),  # integer gene index, determines the point's color
     }
 
-    transcript_points_layer = (
-        coords,
+    layer = (
+        df[["y", "x"]],  # point coordinates
         {
-            "name": "Transcripts",
+            "name": "transcripts",
             "size": point_size,
             "properties": point_properties,
             "face_color": "gene_index",
             "face_color_cycle": COLOR_CYCLE2,
             "edge_width": 0,
         },
-        "points",
     )
-
-    transcript_density_layer: Optional[napari.types.LayerDataTuple]
-
-    if density_plot:
-        density_radius_pixels = math.ceil(
-            density_radius_micrometer / RESOLVE_PIXEL_SIZE
-        )
-        spots = [np.array(coords.iloc[i]) for i in range(selected_df.shape[0])]
-        print("Calculating density map")
-        density = _calculate_density_map(image.data.shape, spots, density_radius_pixels)
-        print(f"max density={np.max(density)}")
-        max_density = np.max(density)
-        if max_density > 0:
-            density_image = density / np.max(density)
-        else:
-            density_image = np.zeros(image.data.shape, dtype=np.uint8)
-        transcript_density_layer = (
-            density_image,
-            {"name": "transcript density", "colormap": "inferno", "opacity": 0.75},
-            "image",
-        )
-    else:
-        transcript_density_layer = None
-
-    layers = []
-    if transcript_density_layer:
-        layers.append(transcript_density_layer)
-    layers.append(transcript_points_layer)
-
-    return layers
+    return layer
 
 
 # Note: we could perhaps instead use scikit-learn's KernelDensity(kernel="linear", ...)
@@ -132,7 +156,9 @@ def transcripts_widget(
 # https://scikit-learn.org/stable/modules/density.html#kernel-density-estimation
 
 
-def _calculate_density_map(map_shape, spots, radius: int):  # radius is in pixels
+def _calculate_density_map(map_shape, spots: np.ndarray, radius: int):
+    # radius is in pixels; spots is a 2D array where each row holds the integer (x,y) coordinates of a spot
+
     # Calculate the kernel, a 2D matrix with the "weight" of each pixel in a spot with given radius.
     kernel = _calculate_kernel(radius)
 
@@ -143,10 +169,8 @@ def _calculate_density_map(map_shape, spots, radius: int):  # radius is in pixel
     # Accumulate the spot density for all spots.
     # (= Add the kernel matrix to a slice of the density map
     # of the same size and at the correct position.)
-    print(f"type of spot is {type(spots[0])} and elem is {type(spots[0][0])}")
     for spot in spots:
-        i: int = spot[0]
-        j: int = spot[1]
+        i, j = spot
         density_map[i : i + kernel.shape[0], j : j + kernel.shape[1]] += kernel
 
     # Return the density map, but with the padding removed again.
