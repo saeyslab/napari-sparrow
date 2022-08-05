088369a5b048e86c30db351edce8d9b705ccea08
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2022-08-05 13:56:51 +0200

    lint

--- src/segment.py
+++ src/segment.py
@@ -6,6 +6,7 @@ import pyrootutils
 from omegaconf import DictConfig
 
 from napari_spongepy import utils
+
 log = utils.get_pylogger(__name__)
 
 # project root setup
@@ -16,25 +17,29 @@ log = utils.get_pylogger(__name__)
 # https://github.com/ashleve/pyrootutils
 root = pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True)
 
+
 def parse_subset(subset):
     """
     e.g $ python src/segment.py subset=\'100,100\'
     >>> parse_subset('100,100')
     (slice(0, 100, 1), slice(0, 100, 1))
     """
-    return tuple([slice(0, int(x), 1) for x in subset.split(',')])
+    return tuple(slice(0, int(x), 1) for x in subset.split(","))
 
-@hydra.main(version_base="1.2", config_path=root / "configs", config_name="segment.yaml")
+
+@hydra.main(
+    version_base="1.2", config_path=root / "configs", config_name="segment.yaml"
+)
 def main(cfg: DictConfig) -> None:
     subset = cfg.subset
     if subset:
         subset = parse_subset(subset)
-        log.info(f'Subset is {subset}')
+        log.info(f"Subset is {subset}")
     # imports can be nested inside @hydra.main to optimize tab completion
     # https://github.com/facebookresearch/hydra/issues/934
     from napari_spongepy._segmentation_widget import _segmentation_worker
 
-    if cfg.segmentation.get('method'):
+    if cfg.segmentation.get("method"):
         method = cfg.segmentation.method
     else:
         method = hydra.utils.instantiate(cfg.segmentation)
@@ -46,9 +51,10 @@ def main(cfg: DictConfig) -> None:
         # small chunks needed if subset is used
     )
     [s, _] = worker.work()
-    output_path = cfg.paths.output_dir+'/segmentation.zarr'
+    output_path = cfg.paths.output_dir + "/segmentation.zarr"
     s.to_zarr(output_path)
     log.info(f"Segmentation saved to {output_path}")
+
 
 if __name__ == "__main__":
     main()
