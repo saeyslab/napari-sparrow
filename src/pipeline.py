1397e40e7ecf88c4a616ffb8f7f0dc50f3ab1a33
KoenDesplenter <koen.desplenter@telenet.be>
2022-08-16 17:09:25 +0200

    change napari util import order

--- src/pipeline.py
+++ src/pipeline.py
@@ -7,9 +7,6 @@ import numpy as np
 import pyrootutils
 from omegaconf import DictConfig
 
-from napari_spongepy import utils
-
-log = utils.get_pylogger(__name__)
 
 # project root setup
 # searches for root indicators in parent dirs, like ".git", "pyproject.toml", etc.
@@ -80,6 +77,9 @@ def main(cfg: DictConfig) -> None:
     from squidpy.im import ImageContainer
 
     import pipelineScripts as pl
+    from napari_spongepy import utils
+
+    log = utils.get_pylogger(__name__)
 
     ic, img = None, None
 
