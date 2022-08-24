7363c5ed6695841f22fe4c0a272197a3f8668aa7
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2022-08-24 09:38:30 +0200

    move ic_to_da

--- src/napari_spongepy/utils/__init__.py
+++ src/napari_spongepy/utils/__init__.py
@@ -1,7 +1,8 @@
 from napari_spongepy.utils.pylogger import get_pylogger
-from napari_spongepy.utils.utils import parse_subset
+from napari_spongepy.utils.utils import ic_to_da, parse_subset
 
 __all__ = [
     "get_pylogger",
     "parse_subset",
+    "ic_to_da",
 ]
