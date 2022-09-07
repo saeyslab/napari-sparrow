87fd4681cdf7af2224218a19144c6d3a4354a116
KoenDesplenter <koen.desplenter@telenet.be>
2022-09-07 09:29:55 +0200

    Segmentation widget without dask

--- src/napari_spongepy/widgets/_segment_widget.py
+++ src/napari_spongepy/widgets/_segment_widget.py
@@ -10,135 +10,91 @@ Setting "Render Images async" is needed to remove jank from rendering.
 """
 
 from enum import Enum
-from typing import Callable
+from typing import Callable, List
 
-import dask.array as da
 import napari
 import napari.layers
 import napari.types
 import numpy as np
-import toolz as tz
-import torch
 from magicgui import magic_factory
 from napari.qt.threading import thread_worker
-from squidpy.im import ImageContainer, segment
 
 import napari_spongepy.utils as utils
 
 log = utils.get_pylogger(__name__)
 
 
-class SegmentationOption(Enum):
-    watershed = "watershed"
-    cellpose = "cellpose"
-    # log = "log"
-    # dog = "dog"
-    # doh = "doh"
+class ModelOption(Enum):
+    nuclei = "nuclei"
+    cyto = "cyto"
 
 
-@tz.curry
-def create_cellpose_method(device: str):
-    from cellpose import models
+def segmentImage(
+    img: np.ndarray,
+    device: str = "cpu",
+    min_size: int = 80,
+    flow_threshold: float = 0.6,
+    diameter: int = 55,
+    cellprob_threshold: int = 0,
+    model_type: str = "nuclei",
+    channels: List[int] = [0, 0],
+) -> np.ndarray:
+    from napari_spongepy.functions import segmentation
 
-    # Needs to be recreated, else AttributeError: 'CPnet' object has no attribute 'diam_mean'
-    model = models.Cellpose(device=torch.device(device), model_type="nuclei")
+    img = np.squeeze(img)
 
-    def cellpose_method(img, fn_kwargs: dict):
-        log.info(f"segmenting {img.shape}")
-        masks, _, _, _ = model.eval(
-            img,
-            channels=[0, 0],
-            **fn_kwargs,
-        )
-        return masks
-
-    return cellpose_method
-
-
-def toggle_layer_vis_on_zoom(viewer, layer_name, zoom_threshold):
-    """
-    Make a layer visible or invisible when the viewer is zoomed in or out.
-    Invisible layers are not rendered or computed.
-    Catch KeyError should layer not be in the LayerList.
-    """
-
-    def f(event, layer_name=layer_name, zoom_threshold=zoom_threshold):
-        try:
-            layer = viewer.layers[layer_name]
-            layer.visible = viewer.camera.zoom > zoom_threshold
-        except KeyError:
-            pass
-
-    return f
-
-
-@thread_worker
-def _segmentation_worker(
-    ic: np.ndarray | ImageContainer,
-    method: Callable | str,
-    reduce_z=None,
-    reduce_c=None,
-    fn_kwargs=None,
-    # if async interactive works: smaller chunks for faster segmentation computation
-    chunks=(1000, 1000, 1, 1),
-    label_image=utils.CLEAN,
-    label_segmentation=utils.SEGMENT,
-) -> list[np.ndarray]:
-
-    if not isinstance(ic, ImageContainer):
-        ic = utils.get_ic(img=ic, layer=label_image, chunks=chunks)
-    segment(
-        ic,
-        method=method,
-        layer=label_image,
-        layer_added=label_segmentation,
-        lazy=True,
-        chunks=chunks,
-        fn_kwargs=fn_kwargs,
+    mask, _, _ = segmentation(
+        img,
+        device,
+        min_size,
+        flow_threshold,
+        diameter,
+        cellprob_threshold,
+        model_type,
+        channels,
     )
-    s = utils.ic_to_da(ic, utils.SEGMENT, reduce_c=reduce_c, reduce_z=reduce_z)
 
-    # make a dummy lower-res array to trigger multi-scale rendering
-    dummy_s = da.zeros(tuple(np.array(s.shape) // 2)).astype(np.uint8)
-    ss = [s, dummy_s]
-    return ss
+    return mask
+
+
+@thread_worker(progress=True)
+def _segmentation_worker(
+    img: np.ndarray,
+    method: Callable,
+    fn_kwargs=None,
+) -> list[np.ndarray]:
+    res = method(img, **fn_kwargs)
+
+    return res
 
 
 @magic_factory(call_button="Segment")
 def segment_widget(
     viewer: napari.Viewer,
     image: napari.layers.Image,
-    method: SegmentationOption = SegmentationOption.watershed,
     device: str = "cpu",
     min_size: int = 80,
-    flow_threshold: float = 0.6,
-    diameter: int = 55,
-    cellprob_threshold: int = 0,
-    thresh: int = 10_000,
-    geq: bool = True,
+    flow_threshold: float = 0.8,
+    diameter: int = 50,
+    cellprob_threshold: int = -2,
+    model_type: ModelOption = ModelOption.nuclei,
 ) -> None:
 
-    log.info(f"About to segment {image} using {method}; device={device}")
+    log.info(f"About to segment {image} using cellpose; device={device}")
     if image is None:
         return
-    if method == SegmentationOption.cellpose:
-        method_fn = create_cellpose_method(device)
+    else:
+        method_fn = segmentImage
         fn_kwargs = {
+            "device": device,
             "min_size": min_size,
             "flow_threshold": flow_threshold,
             "diameter": diameter,
             "cellprob_threshold": cellprob_threshold,
-            "resample": False,
+            "model_type": model_type.value,
+            "channels": [0, 0],
         }
-    else:
-        method_fn = method.value
-        fn_kwargs = {
-            "thresh": thresh,
-            "geq": geq,
-        }
-    worker = _segmentation_worker(
-        image.data, method_fn, fn_kwargs=fn_kwargs, label_image=image.name
-    )
+    worker = _segmentation_worker(image.data, method_fn, fn_kwargs=fn_kwargs)
     log.info("Worker created")
 
     layer_name = utils.SEGMENT
@@ -149,22 +105,13 @@ def segment_widget(
             layer = viewer.layers[layer_name]
             viewer.layers.remove(layer)
 
-            # layer.data = img
             log.info(f"Refreshing {layer_name}")
-            # layer.refresh()
         except KeyError:
             # otherwise add it to the viewer
             log.info(f"Adding {layer_name}")
         viewer.add_labels(img, visible=True, name=layer_name)
-        # f = toggle_layer_vis_on_zoom(viewer, layer_name, zoom_threshold=0.9)
-        # viewer.camera.events.zoom.connect(f)
-        # execute f to emulate zoom event and set visiblity correct
-        # f(None)
-        log.debug(print(utils.get_ic()))
+
         return viewer
 
     worker.returned.connect(add_labels)
-    # worker.returned.connect(
-    #     lambda label_img: viewer.add_labels(label_img, name="Segmentation")
-    # )
     worker.start()
