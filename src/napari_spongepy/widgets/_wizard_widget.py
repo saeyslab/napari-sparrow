a15ed2eabec993d5140704dbac1c8ee916dc041f
KoenDesplenter <koen.desplenter@telenet.be>
2022-09-08 16:11:41 +0200

    Better step title

--- src/napari_spongepy/widgets/_wizard_widget.py
+++ src/napari_spongepy/widgets/_wizard_widget.py
@@ -2,13 +2,11 @@
 Napari widget for managing the other widgets and giving a general overview of the workflow.
 """
 
-from enum import Enum
-
 from magicgui.widgets import ComboBox, Container, TextEdit
 from qtpy.QtWidgets import QSizePolicy
 
 from napari_spongepy.utils import get_pylogger
-from napari_spongepy.widgets import (  # load_widget,
+from napari_spongepy.widgets import (
     allocate_widget,
     annotate_widget,
     clean_widget,
@@ -29,47 +27,67 @@ class Step:
     def __str__(self):
         return self.name
 
-
-class StepOption(Enum):
-    clean = Step(
-        "clean",
-        "#1 Clean",
-        clean_widget,
-        "#1 Cleaning step:\nThis step performs lighting correction and mask inpainting for the black lines, afterwards a tophat filter and contrast clip are applied.",
-    )
-    segment = Step(
-        "segment", "#2 Segment", segment_widget, "#2 Segmentation step description"
-    )
-    allocate = Step(
-        "Allocate", "#3 Allocate", allocate_widget, "#3 Allocation step description"
-    )
-    annotate = Step(
-        "Annotate", "#4 Annotate", annotate_widget, "#4 Annotation step description"
-    )
-    visualize = Step(
-        "Visualize",
-        "#5 Visualize",
-        visualize_widget,
-        "#5 Visualisation step description",
-    )
-
-    def __repr__(self):
-        return self.value
-
-    def __str__(self):
-        return str(self.value)
-
     def get_widget(self):
-        widget = self.value.widget()
-        widget.name = str(self.value)
+        widget = self.widget()
+        widget.name = self.name
         return widget
 
     def get_description(self):
         description = TextEdit(
-            value=self.value.description, name=str(self) + "description", enabled=False
+            value=self.description, name=self.name + "description", enabled=False
         )
         description.native.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
         return description
+
+
+def get_choices():
+    return [
+        (
+            "Step 1: Clean",
+            Step(
+                "clean",
+                "#1 Clean",
+                clean_widget,
+                "#1 Cleaning step:\nThis step performs lighting correction and mask inpainting for the black lines, afterwards a tophat filter and contrast clip are applied.",
+            ),
+        ),
+        (
+            "Step 2: Segment",
+            Step(
+                "segment",
+                "#2 Segment",
+                segment_widget,
+                "#2 Segmentation step description",
+            ),
+        ),
+        (
+            "Step 3: Allocate",
+            Step(
+                "Allocate",
+                "#3 Allocate",
+                allocate_widget,
+                "#3 Allocation step description",
+            ),
+        ),
+        (
+            "Step 4: Annotate",
+            Step(
+                "Annotate",
+                "#4 Annotate",
+                annotate_widget,
+                "#4 Annotation step description",
+            ),
+        ),
+        (
+            "Step 5: Visualize",
+            Step(
+                "Visualize",
+                "#5 Visualize",
+                visualize_widget,
+                "#5 Visualisation step description",
+            ),
+        ),
+    ]
 
 
 def wizard_widget() -> None:
@@ -77,21 +95,17 @@ def wizard_widget() -> None:
     Napari widget for managing the other widgets and giving a general overview of the workflow.
     TODO add next step button
     """
-    # log.debug(step)
 
-    step = ComboBox(label="Step:", choices=StepOption, name="step")
+    step = ComboBox(label="Step:", choices=get_choices(), name="step")
     container = Container(
         name="global",
         widgets=[
             step,
-            StepOption.clean.get_description(),
-            StepOption.clean.get_widget(),
+            get_choices()[0][1].get_description(),
+            get_choices()[0][1].get_widget(),
         ],
         labels=False,
     )
-
-    # global current_widget
-    # current_widget = clean
 
     def step_changed(event):
         """This is a callback that updates the current step
@@ -114,7 +128,3 @@ def wizard_widget() -> None:
     step.changed.connect(step_changed)
 
     return container
-
-
-if __name__ == "__main__":
-    print(123)
