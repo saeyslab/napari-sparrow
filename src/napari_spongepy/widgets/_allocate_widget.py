268026c2695919a89ed3f8cc5798dd924ebca91e
KoenDesplenter <koen.desplenter@telenet.be>
2022-09-08 11:40:09 +0200

    Added thread worker

--- src/napari_spongepy/widgets/_allocate_widget.py
+++ src/napari_spongepy/widgets/_allocate_widget.py
@@ -2,22 +2,33 @@
 Allocation widget for creating and preprocesing the adata object, filtering the cells and performing clustering.
 """
 import pathlib
+from typing import Callable
 
 import napari
 import napari.layers
 import napari.types
+import numpy as np
+from anndata import AnnData
 from magicgui import magic_factory
 from napari.qt.threading import thread_worker
-import numpy as np
-from typing import Callable
-from anndata import AnnData
+
 import napari_spongepy.utils as utils
 from napari_spongepy import functions as fc
 
 log = utils.get_pylogger(__name__)
 
 
-def allocateImage(path: str, img: np.ndarray, masks: np.ndarray, pcs: int, neighbors: int, library_id: str = "melanoma", min_size: int = 100, max_size: int = 100000, cluster_resolution: float = 0.8) -> AnnData:
+def allocateImage(
+    path: str,
+    img: np.ndarray,
+    masks: np.ndarray,
+    pcs: int,
+    neighbors: int,
+    library_id: str = "melanoma",
+    min_size: int = 100,
+    max_size: int = 100000,
+    cluster_resolution: float = 0.8,
+) -> AnnData:
     adata = fc.create_adata_quick(path, img, masks, library_id)
     adata, _ = fc.preprocessAdata(adata, masks)
     adata, _ = fc.filter_on_size(adata, min_size, max_size)
@@ -37,7 +48,8 @@ def _allocation_worker(
 
 @magic_factory(
     call_button="Allocate",
-    transcripts_file={"widget_type": "FileEdit", "filter": "*.txt"}, result_widget=True
+    transcripts_file={"widget_type": "FileEdit", "filter": "*.txt"},
+    result_widget=True,
 )
 def allocate_widget(
     viewer: napari.Viewer,
@@ -69,7 +81,7 @@ def allocate_widget(
         "library_id": library_id,
         "min_size": min_size,
         "max_size": max_size,
-        "cluster_resolution": cluster_resolution
+        "cluster_resolution": cluster_resolution,
     }
 
     worker = _allocation_worker(allocateImage, fn_kwargs)
@@ -82,11 +94,11 @@ def allocate_widget(
         except KeyError:
             # otherwise add it to the viewer
             log.info(f"Layer does not exist {utils.SEGMENT}")
-        
+
         layer.metadata["adata_allocate"] = result
         log.info("Allocation finished")
-        
-        return "Allocation finished" 
+
+        return "Allocation finished"
 
     worker.returned.connect(add_metadata)
     worker.start()
