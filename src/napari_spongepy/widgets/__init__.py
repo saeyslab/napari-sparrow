b1bce2e8c87f719324d3a6d0fddeb447f71dc4e5
KoenDesplenter <koen.desplenter@telenet.be>
2022-09-16 11:43:59 +0200

    chore: removed unused load widget

--- src/napari_spongepy/widgets/__init__.py
+++ src/napari_spongepy/widgets/__init__.py
@@ -1,15 +1,14 @@
+"""Export all the widgets for the napari plugin."""
 __version__ = "0.0.1"
 
 from napari_spongepy.widgets._allocate_widget import allocate_widget
 from napari_spongepy.widgets._annotate_widget import annotate_widget
 from napari_spongepy.widgets._clean_widget import clean_widget
-from napari_spongepy.widgets._load_widget import load_widget
 from napari_spongepy.widgets._segment_widget import segment_widget
 from napari_spongepy.widgets._visualize_widget import visualize_widget
 from napari_spongepy.widgets._wizard_widget import wizard_widget
 
 __all__ = [
-    load_widget,
     clean_widget,
     segment_widget,
     wizard_widget,
