a485d4cb900373bc28176fe7666bfa967c52986b
ArneDefauw <arnedefauw@gmail.com>
2023-09-27 11:47:05 +0200

    SC_24 add apply function

--- src/napari_sparrow/image/__init__.py
+++ src/napari_sparrow/image/__init__.py
@@ -3,3 +3,4 @@ from ._tiling import tiling_correction
 from ._contrast import enhance_contrast
 from ._minmax import min_max_filtering
 from ._transcripts import transcript_density
+from ._apply import apply, ChannelList
