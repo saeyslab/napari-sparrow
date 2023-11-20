3fcd996a6777427883e17ee8671b7c9f4aa7fb9b
ArneDefauw <arnedefauw@gmail.com>
2023-11-20 16:38:27 +0100

    SC_41 apply function refactor

--- src/napari_sparrow/image/_apply.py
+++ src/napari_sparrow/image/_apply.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from types import MappingProxyType
-from typing import Any, Callable, Iterable, Mapping, Optional, Tuple
+from typing import Any, Callable, Iterable, Mapping, Optional, Tuple, Dict, List
 
 import dask.array as da
 from dask.array import Array
@@ -132,7 +132,7 @@ def apply(
             if len(chunks) != arr.ndim:
                 raise ValueError(
                     f"Chunks ({chunks}) are provided for {len(chunks)} dimensions. "
-                    f"Please (only) provide chunks for the {arr.ndim} spatial dimensions."
+                    f"Please (only) provide chunks for {arr.ndim} dimensions."
                 )
         arr = da.asarray(arr).rechunk(chunks)
         if "depth" in kwargs:
@@ -155,7 +155,7 @@ def apply(
                 if len(depth) != arr.ndim:
                     raise ValueError(
                         f"Depth ({depth}) is provided for {len(depth)} dimensions. "
-                        f"Please (only) provide depth for the {arr.ndim} spatial dimensions."
+                        f"Please (only) provide depth for {arr.ndim} dimensions."
                     )
             depth = coerce_depth(arr.ndim, depth)
 
@@ -183,76 +183,103 @@ def apply(
         channel = se.c.data
 
     # here you specify the z-stacks the function is applied on
-    if z_slice is not None:
-        # TODO check if this makes sense, i.e. se should be 3 dimensional
-        z_slice = (
-            list(z_slice)
-            if isinstance(z_slice, Iterable) and not isinstance(z_slice, str)
-            else [z_slice]
-        )
+    if "z" in se.dims:
+        if z_slice is not None:
+            z_slice = (
+                list(z_slice)
+                if isinstance(z_slice, Iterable) and not isinstance(z_slice, str)
+                else [z_slice]
+            )
+        else:
+            z_slice = se.z.data
     else:
-        z_slice = se.z.data
+        z_slice = None
+        log.info(
+            f"combine_z flag was set to False, while {se} does not contain a 'z' dimension."
+        )
+        # trivial combine_z if there is not z dimension
+        combine_z = True
 
-    # sanity check on 3D. If no z-dimension, set combine_z to True
-    if 'z' not in se.dims:
-        # set combine_z trivial to True if there is no z dimension
-        combine_z=True
-
-    fn_kwargs, func=_precondition( fn_kwargs=fn_kwargs, func=func, combine_c=combine_c, combine_z=combine_z, channels=channel, z_slices=z_slice )
+    fn_kwargs, func = _precondition(
+        fn_kwargs=fn_kwargs,
+        func=func,
+        combine_c=combine_c,
+        combine_z=combine_z,
+        channels=channel,
+        z_slices=z_slice,
+    )
 
     if not combine_c:
-        # so process them all indepentenly
+        # process channels dimensions indepentenly, so get channel names from fn_kwargs, because could have less channels
+        channel = list(fn_kwargs.keys())
         # TODO not allow is se is not 3D (fix this by setting combine_z to True if se does not contain z dimensions)
         if not combine_z:
-            result=[]
+            result = []
             for key_c, _fn_kwargs_c in fn_kwargs.items():
-                func_c=func[ key_c ]
-                result_z=[]
+                func_c = func[key_c]
+                result_z = []
                 for key_z, _fn_kwargs_c_z in _fn_kwargs_c.items():
-                    func_c_z=func_c[ key_z ]
-                    arr=se.sel( z=[key_z] , c=[key_c] ).data  # we do not reduce dimensions, we do sel with z=[...]
+                    func_c_z = func_c[key_z]
+                    arr = se.sel(
+                        z=[key_z], c=[key_c]
+                    ).data  # we do not reduce dimensions, we do sel with z=[...]
                     if crd is not None:
-                        arr = arr[ : ,:, crd[2] : crd[3], crd[0] : crd[1]]
-                    arr=apply_func( func=func_c_z, arr=arr, fn_kwargs=_fn_kwargs_c_z )
+                        arr = arr[:, :, crd[2] : crd[3], crd[0] : crd[1]]
+                    arr = apply_func(func=func_c_z, arr=arr, fn_kwargs=_fn_kwargs_c_z)
                     # arr of size (1,1,y,x)
-                    result_z.append( arr[ 0, 0, ... ] )
-                result.append(da.stack( result_z, axis=0 ))
-            arr=da.stack( result, axis=0 )
+                    result_z.append(arr[0, 0, ...])
+                result.append(da.stack(result_z, axis=0))
+            arr = da.stack(result, axis=0)
         else:
             # this combines z. Thus all z dimensions are send to apply func
-            result=[]
+            result = []
             for key_c, _fn_kwargs_c in fn_kwargs.items():
-                func_c=func[ key_c ]
-                arr=se.sel( c=[key_c] ).data  # we do not reduce dimensions, we do sel with c=[...]
+                func_c = func[key_c]
+                # TODO should only do se.sel( z=... ) if there is a z dimension
+                if z_slice is not None:
+                    arr = se.sel(
+                        z=z_slice, c=[key_c]
+                    ).data  # we do not reduce dimensions, we do sel with c=[...]
+                else:
+                    arr = se.sel(
+                        c=[key_c]
+                    ).data  # we do not reduce dimensions, we do sel with c=[...]
                 if crd is not None:
                     if arr.ndim == 3:
-                        arr = arr[ : , crd[2] : crd[3], crd[0] : crd[1]]
-                    elif arr.ndim ==4:
-                        arr = arr[ : , :, crd[2] : crd[3], crd[0] : crd[1]]
-                arr=apply_func( func=func_c, arr=arr, fn_kwargs=_fn_kwargs_c ) # apply func expects c,z,y,x
-                # this returns 1, z, y, x, so we take z,y,x and do a stack over all results to get our c-dimension back
-                result.append( arr[ 0, ... ] )
-            arr=da.stack( result, axis=0 )
+                        arr = arr[:, crd[2] : crd[3], crd[0] : crd[1]]
+                    elif arr.ndim == 4:
+                        arr = arr[:, :, crd[2] : crd[3], crd[0] : crd[1]]
+                arr = apply_func(
+                    func=func_c, arr=arr, fn_kwargs=_fn_kwargs_c
+                )  # apply func expects c,z,y,x
+                # apply_func returns 1, z, y, x, so we take z,y,x and do a stack over all results to get our c-dimension back
+                result.append(arr[0, ...])
+            arr = da.stack(result, axis=0)
     else:
         if not combine_z:
-            # TODO not allow is se is not 3D (fix this by setting combine_z to True if se does not contain z dimensions)
-            result=[]
+            result = []
             for key_z, _fn_kwargs_z in fn_kwargs.items():
-                func_z=func[ key_z ]
-                arr=se.sel( c=[key_z] ).data
+                func_z = func[key_z]
+                arr = se.sel(z=[key_z], c=channel).data
                 if crd is not None:
-                    arr = arr[ : ,:, crd[2] : crd[3], crd[0] : crd[1]]
-                arr=apply_func( func=func_z, arr=arr, fn_kwargs=_fn_kwargs_z )
+                    arr = arr[:, :, crd[2] : crd[3], crd[0] : crd[1]]
+                arr = apply_func(func=func_z, arr=arr, fn_kwargs=_fn_kwargs_z)
+                # apply func returns c,1,y,x, so we take c,y,x and do stack over results to get z-dimension back
+                result.append(arr[:, 0, ...])
+            arr = da.stack(result, axis=1)
 
-        else: # case where combine_z is True, and combine_c is True 
-            # -> just regular fn_kwargs and func passed to apply, but on channels that are specified
-            arr=se.sel( c=[channel], z=[z_slice] ).data
+        else:  # case where combine_z is True, and combine_c is True
+            # -> just regular fn_kwargs and func passed to apply, but on channels and z_slice that are specified
+            if z_slice is not None:
+                arr = se.sel(c=channel, z=z_slice).data
+            else:
+                arr = se.sel(c=channel).data
             if crd is not None:
                 if arr.ndim == 3:
-                    arr = arr[ : , crd[2] : crd[3], crd[0] : crd[1]]
-                elif arr.ndim ==4:
-                    arr = arr[ : , :, crd[2] : crd[3], crd[0] : crd[1]]
-            arr=apply_func( func=func, arr=arr, fn_kwargs=fn_kwargs )
+                    arr = arr[:, crd[2] : crd[3], crd[0] : crd[1]]
+                elif arr.ndim == 4:
+                    arr = arr[:, :, crd[2] : crd[3], crd[0] : crd[1]]
+            arr = apply_func(func=func, arr=arr, fn_kwargs=fn_kwargs)
 
     if crd is not None:
         crd = _substract_translation_crd(se, crd)
@@ -279,31 +306,41 @@ def apply(
     return sdata
 
 
-def _precondition( fn_kwargs, func, combine_c, combine_z, channels, z_slices ):
-
+def _precondition(
+    fn_kwargs: Dict[Any:Any],
+    func: Callable | Dict[Any:Any],
+    combine_c: bool,
+    combine_z: bool,
+    channels: List[str] | List[int],
+    z_slices: List[float],
+):
     def collect_keys_fn_kwargs(d):
+        if d == {}:
+            return
         keys = []
         for key, value in d.items():
             keys.append(key)
-            if isinstance(value,  (dict) ):
-                result=collect_keys_fn_kwargs( value )
+            if isinstance(value, (dict)):
+                result = collect_keys_fn_kwargs(value)
                 if result is not None:
-                    keys.extend( result )
+                    keys.append(result)
             else:
                 return
         return keys
 
     def collect_keys_func(d):
+        if d == {}:
+            return
         keys = []
         for key, value in d.items():
             keys.append(key)
-            if isinstance(value,  (dict) ):
-                result=collect_keys_func( value )
+            if isinstance(value, (dict)):
+                result = collect_keys_func(value)
                 if result is not None:
-                    keys.extend( result )
+                    keys.append(result)
             else:
-                if not isinstance( value, Callable ):
-                    raise ValueError( "Should specify callable if a dict is provided." )
+                if not isinstance(value, Callable):
+                    raise ValueError("Should specify callable if a dict is provided.")
         return keys
 
     def flatten_list(nested_list):
@@ -314,7 +351,6 @@ def _precondition( fn_kwargs, func, combine_c, combine_z, channels, z_slices ):
             else:
                 result.append(element)
         return result
-
 
     def make_mapping(keys, value):
         result = {}
@@ -327,63 +363,139 @@ def _precondition( fn_kwargs, func, combine_c, combine_z, channels, z_slices ):
                 previous_item = item
         return result
 
+    if fn_kwargs == {}:
+        if isinstance(func, Callable):
+            if not combine_z:
+                if fn_kwargs == {}:
+                    fn_kwargs = {key: fn_kwargs for key in z_slices}
+            if not combine_c:
+                fn_kwargs = {key: fn_kwargs for key in channels}
+        else:
+            keys_func = collect_keys_func(func)
+            fn_kwargs = make_mapping(keys_func, {})
 
-    keys_fn_kwargs=collect_keys_fn_kwargs( fn_kwargs )
-    if isinstance( func, Callable  ):
+    keys_fn_kwargs = collect_keys_fn_kwargs(fn_kwargs)
+    if isinstance(func, Callable):
         if keys_fn_kwargs is not None:
-            # if callable, make a mapping of it
-            func=make_mapping( keys_fn_kwargs, func )
+            # if callable, and parameters are specified for specific channels/z-stacks make a mapping of func
+            func = make_mapping(keys_fn_kwargs, func)
 
     # sanity checks
     if keys_fn_kwargs is not None:
-        keys_func=collect_keys_func( func )
-        assert keys_fn_kwargs == keys_func, "should specify same keys in fn_kwargs and func"
-        keys=set(flatten_list( keys_fn_kwargs ))
+        keys_func = collect_keys_func(func)
+        assert (
+            keys_fn_kwargs == keys_func
+        ), "should specify same keys in fn_kwargs and func"
+        keys = set(flatten_list(keys_fn_kwargs))
         # now also do sanity check on the channels and z slices.
         if combine_c:
-            if keys.intersection( set(channels) ):
-                raise ValueError( "keys in fn_kwargs also present in channels can not be specified if process_each_channel_indepentenlyt is set to false." )
+            if keys.intersection(set(channels)):
+                raise ValueError(
+                    "Keys in fn_kwargs can not have intersection with channel names if combine_c is set to True."
+                )
         if combine_z:
-            if keys.intersection( set(z_slices) ):
-                raise ValueError( "keyword argumens for z_slices can not be specified if process_each_z_stack_indepentenlyt is set to false." )
+            if keys.intersection(set(z_slices)):
+                raise ValueError(
+                    "Keys in fn_kwargs can not have intersection with names of z_slices if combine_z is set to True."
+                )
 
     # fix keys
     if not combine_c:
-        if not set(fn_kwargs.keys()).issubset( set( channels ) ):
+        if not (
+            set(fn_kwargs.keys()).issubset(set(channels))
+            or set(channels).issubset(set(fn_kwargs.keys()))
+        ):
             if not combine_z:
-                if not set(fn_kwargs.keys()).issubset( set( z_slices ) ):
+                if not (
+                    set(fn_kwargs.keys()).issubset(set(z_slices))
+                    or set(z_slices).issubset(fn_kwargs.keys())
+                ):
                     # in this case func should be a callable
-                    #if not isinstance( func, Callable ): 
+                    # if not isinstance( func, Callable ):
                     #    raise ValueError
-                    print( "process each zstack indepentenlty is True, but not all z-slices spefified in fn_kwargs/func." )
-                    fn_kwargs={key: fn_kwargs for key in z_slices }
-                    func={key: func for key in z_slices }
-            fn_kwargs={key: fn_kwargs for key in channels } 
-            func={key: func for key in channels }
-            
+                    print(
+                        "combine_z is False, but not all z-slices spefified in fn_kwargs/func."
+                    )
+                    fn_kwargs = {key: fn_kwargs for key in z_slices}
+                    func = {key: func for key in z_slices}
+            print(
+                "combine_c is False, but not all channels spefified in fn_kwargs/func."
+            )
+            fn_kwargs = {key: fn_kwargs for key in channels}
+            func = {key: func for key in channels}
+
         # case where we are subset of channels, but want to add z dimension
         elif not combine_z:
-            for item, value in fn_kwargs.items():
-                if not set(value.keys()).issubset( set( z_slices ) ):
-                    print( "process each zstack indepentenlty is True, but not all z-slices spefified in fn_kwargs/func" )
+            for _, value in fn_kwargs.items():
+                if (
+                    not (
+                        set(value.keys()).issubset(set(z_slices))
+                        or set(z_slices).issubset(set(value.keys()))
+                    )
+                    or value == {}
+                ):
+                    print(
+                        "combine_z is False, but not all z-slices spefified in fn_kwargs/func"
+                    )
                     # print a warning
-                    fn_kwargs = {key: {z_slice: _value for z_slice in z_slices} for key, _value in fn_kwargs.items()}
-                    func = {key: {z_slice: _value for z_slice in z_slices} for key, _value in func.items()}
+                    fn_kwargs = {
+                        key: {z_slice: _value for z_slice in z_slices}
+                        for key, _value in fn_kwargs.items()
+                    }
+                    func = {
+                        key: {z_slice: _value for z_slice in z_slices}
+                        for key, _value in func.items()
+                    }
                     break
     elif not combine_z:
-        print( "process each zstack indepentenlty is True, but not all z-slices spefified in fn_kwargs/func" )
-        if not set(fn_kwargs.keys()).issubset( set( z_slices ) ):
-            fn_kwargs={key: fn_kwargs for key in z_slices }
-            func={key: func for key in z_slices }
+        if not (
+            set(fn_kwargs.keys()).issubset(set(z_slices))
+            or set(z_slices).issubset(set(fn_kwargs.keys()))
+        ):
+            print(
+                "combine_z is False, but not all z-slices spefified in fn_kwargs/func"
+            )
+            fn_kwargs = {key: fn_kwargs for key in z_slices}
+            func = {key: func for key in z_slices}
     else:
-        assert isinstance( func, Callable )
-        assert keys_fn_kwargs is None, "process_each_channel_indepetnently and process_each_z_stack_indepe are both set to False, but it seems fn_kwargs contains specific values for channels and z stakcs"
-        
+        assert isinstance(func, Callable)
+        assert (
+            keys_fn_kwargs is None
+        ), "combine_z and combine_c are both set to True, but it seems fn_kwargs contains specific parameters for different channels and z stacks."
+
     return fn_kwargs, func
 
 
+class ChannelList:
+    def __init__(self, *args, **kwargs):
+        self._list = list(*args, **kwargs)
 
-'''
+    def __len__(self):
+        return len(self._list)
+
+    def __getitem__(self, index):
+        return self._list[index]
+
+    def __setitem__(self, index, value):
+        self._list[index] = value
+
+    def __delitem__(self, index):
+        del self._list[index]
+
+    def append(self, item):
+        self._list.append(item)
+
+    def extend(self, items):
+        self._list.extend(items)
+
+    def remove(self, item):
+        self._list.remove(item)
+
+    def __repr__(self):
+        return repr(self._list)
+
+
+"""
 def _precondition( fn_kwargs, func ):
 
     # sanity check:
@@ -439,4 +551,4 @@ def _precondition( fn_kwargs, func ):
 
     else:
         pass
-'''
+"""
