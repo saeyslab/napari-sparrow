bb5340fa347bc1cca331ea9231257b694de802ea
ArneDefauw <arnedefauw@gmail.com>
2023-12-04 16:37:52 +0100

    SC_44 mesmer issue

--- src/napari_sparrow/image/segmentation/segmentation_models/_mesmer.py
+++ src/napari_sparrow/image/segmentation/segmentation_models/_mesmer.py
@@ -32,7 +32,7 @@ def _mesmer(
     if nuc_channel is None or mem_channel is None:
         specified_channel = nuc_channel if nuc_channel is not None else mem_channel
         channel_img = img[..., specified_channel][..., None]
-        zeros_arr = np.zeros((img.shape[0], img.shape[1], 1))
+        zeros_arr = np.zeros((1, img.shape[1], img.shape[2], 1))
         img = np.concatenate((channel_img, zeros_arr), axis=-1)
 
     # If both channels are specified
@@ -43,7 +43,25 @@ def _mesmer(
 
     # mesmer want img to be of dimension (batch, y, x, c)
     # we give it input (z, y, x, c), with z dim ==1.
+
+    # Mesmer (sometimes) raises errors when img.shape[1]!=img.shape[2], therefore pad with 0
+    pad_y = 0
+    pad_x = 0
+    if img.shape[2] > img.shape[1]:
+        pad_y = (img.shape[2] - img.shape[1]) // 2
+        pad_size = ((0, 0), (pad_y, pad_y), (0, 0), (0, 0))
+        img = np.pad(img, pad_width=pad_size, mode="constant", constant_values=0)
+    elif img.shape[1] > img.shape[2]:
+        pad_x = (img.shape[1] - img.shape[2]) // 2
+        pad_size = ((0, 0), (0, 0), (pad_x, pad_x), (0, 0))
+        img = np.pad(img, pad_width=pad_size, mode="constant", constant_values=0)
+
     masks = app.predict(img, image_mpp=image_mpp, compartment=compartment)
+
+    if pad_y:
+        masks = masks[:, pad_y:-pad_y, :, :]
+    if pad_x:
+        masks = masks[:, :, pad_x:-pad_x, :]
 
     # mesmer returns dimension (batch, y,x,c), with in our use case batch==1, and c==1.
     # so it is in correct format (z,y,x,c)
