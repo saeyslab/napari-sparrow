44a1e7017792190e01bb794f716b2b8366ba507c
ArneDefauw <arnedefauw@gmail.com>
2024-04-17 16:26:47 +0200

    SC_6 add unit test pixel matrix

--- src/sparrow/_tests/test_table/test_pixel_clustering/test_create_pixel_matrix.py
+++ src/sparrow/_tests/test_table/test_pixel_clustering/test_create_pixel_matrix.py
@@ -1,6 +1,30 @@
 import numpy as np
 
-from sparrow.table.pixel_clustering._create_pixel_matrix import _sampling_function
+from sparrow.table.pixel_clustering._create_pixel_matrix import _sampling_function, create_pixel_matrix
+
+
+def test_create_create_pixel_matrix(sdata_multi_c):
+    sdata_multi_c = create_pixel_matrix(
+        sdata_multi_c,
+        img_layer=["raw_image"],
+        output_layer="table_pixels",
+        channels=[2, 5, 7, 20],
+        q=99,
+        q_sum=5,
+        q_post=99.9,
+        sigma=2.0,
+        norm_sum=True,
+        fraction=0.2,
+        chunks=512,
+        seed=10,
+        overwrite=True,
+    )
+    assert "table_pixels" in sdata_multi_c.tables
+    assert sdata_multi_c.tables["table_pixels"].shape == (52088, 4)
+    assert np.allclose(
+        sdata_multi_c["table_pixels"].var["raw_image_post_norm_percentile_99.9"].values,
+        np.array([0.98521747, 0.9910488, 0.92450532, 0.99695877]),
+    )
 
 
 def test_sampling_function():
@@ -17,4 +41,4 @@ def test_sampling_function():
         ]
     )
 
-    assert np.allclose(result_array, correct_array, equal_nan=True)
+    assert np.array_equal(result_array, correct_array, equal_nan=True)
