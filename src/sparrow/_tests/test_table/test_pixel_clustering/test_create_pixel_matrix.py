080289b64dea788e106f6e98f1957d0cf5d45097
ArneDefauw <arnedefauw@gmail.com>
2024-04-23 13:46:10 +0200

    SC_6 integration with flowsom

--- src/sparrow/_tests/test_table/test_pixel_clustering/test_create_pixel_matrix.py
+++ src/sparrow/_tests/test_table/test_pixel_clustering/test_create_pixel_matrix.py
@@ -7,6 +7,7 @@ def test_create_pixel_matrix(sdata_multi_c):
     sdata_multi_c = create_pixel_matrix(
         sdata_multi_c,
         img_layer=["raw_image"],
+        output_img_layer=["raw_image_preprocessed"],
         output_table_layer="table_pixels",
         channels=[2, 5, 7, 20],
         q=99,
@@ -20,7 +21,9 @@ def test_create_pixel_matrix(sdata_multi_c):
         overwrite=True,
     )
     assert "table_pixels" in sdata_multi_c.tables
+    assert "raw_image_preprocessed" in sdata_multi_c.images
     assert sdata_multi_c.tables["table_pixels"].shape == (52099, 4)
+    # check if we did not sample nans
     assert sdata_multi_c.tables["table_pixels"].to_df().isna().any(axis=0).sum() == 0
     assert np.allclose(
         sdata_multi_c["table_pixels"].var["raw_image_post_norm_percentile_99.9"].values,
--- src/sparrow/table/__init__.py
+++ src/sparrow/table/__init__.py
@@ -1,8 +1,17 @@
+from sparrow.utils.pylogger import get_pylogger
+
 from ._allocation import allocate
 from ._allocation_intensity import allocate_intensity
 from ._annotation import cluster_cleanliness, score_genes
-from ._clustering import kmeans, leiden, flowsom
+from ._clustering import kmeans, leiden
 from ._enrichment import nhood_enrichment
 from ._preprocess import preprocess_proteomics, preprocess_transcriptomics
 from ._regionprops import add_regionprop_features
 from ._table import correct_marker_genes, filter_on_size
+
+log = get_pylogger(__name__)
+
+try:
+    from ._clustering import flowsom
+except ImportError:
+    log.warning("'flowsom' not installed, 'sp.tb.flowsom' will not be available.")
