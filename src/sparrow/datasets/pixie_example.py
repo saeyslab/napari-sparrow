feaa2ef0a6c1ce95d054dbbaae77de177411422f
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2024-06-06 10:27:58 +0200

    add labels to dataset

--- src/sparrow/datasets/pixie_example.py
+++ src/sparrow/datasets/pixie_example.py
@@ -1,13 +1,42 @@
+import anndata as ad
+import pandas as pd
 from spatialdata import SpatialData
 
 
-def pixie_example() -> SpatialData:
+def get_df(path):
+    df = pd.read_csv(path, index_col=0).reset_index()
+    return df
+
+
+def get_adata(path, label_prefix="label_whole_"):
+    df = get_df(path)
+    df["fov"] = label_prefix + df["fov"]
+    obs_columns: list[str] = ["index", "cell_size"] + df.columns[24:].to_list()
+    obs = df.loc[:, df.columns.isin(obs_columns)]
+    obs = obs.astype(
+        {
+            "index": "int64",
+            "fov": "category",
+            "cell_meta_cluster": "category",
+            "label": "int64",
+        }
+    )
+    X = df.loc[:, ~df.columns.isin(obs_columns)]
+    adata = ad.AnnData(
+        X=X,
+        obs=obs,
+    )
+    return adata
+
+
+def pixie_example(fovs: list | None = None, with_pixel_output=True, with_cells_output=True) -> SpatialData:
     """Example pixie dataset."""
     import glob
     import os
     from pathlib import Path
 
     import dask.array as da
+    import spatialdata as sd
     from dask_image import imread
     from datasets import load_dataset
 
@@ -20,14 +49,37 @@ def pixie_example() -> SpatialData:
 
     path_image_data = Path(dataset["cluster_cells"]["image_data"][0]) / "image_data"
 
+    # if with_pixel_output:
+    #     path_pixel_data = Path(dataset["cluster_cells"]["example_pixel_output_dir"][0]) / "example_pixel_output_dir"
+
+    if with_cells_output:
+        # dataset = load_dataset("angelolab/ark_example", "post_clustering", trust_remote_code=True)
+        # path_cell_data = Path(dataset["post_clustering"]["example_cell_output_dir"][0]) / "example_cell_output_dir"
+        dataset = load_dataset("angelolab/ark_example", "pairwise_spatial_enrichment", trust_remote_code=True)
+        path_post_data = Path(dataset["pairwise_spatial_enrichment"]["post_clustering"][0]) / "post_clustering"
+
     assert path_segment_data.exists(), path_segment_data
 
     assert path_image_data.exists(), path_image_data
 
-    sdata = SpatialData()
+    # if with_pixel_output:
+    #     assert path_pixel_data.exists(), path_pixel_data
+    #     print(path_pixel_data)
 
-    for i in [0, 1]:
-        path = os.path.join(path_image_data, f"fov{i}")
+    if with_cells_output:
+        # assert path_cell_data.exists(), path_cell_data
+        # print(path_cell_data)
+        assert path_post_data.exists(), path_post_data
+        print(path_post_data)
+
+    if fovs is None:
+        fovs = range(11)
+    if isinstance(fovs[0], int):
+        fovs = [f"fov{i}" for i in fovs]
+
+    sdata = SpatialData()
+    for fov in fovs:
+        path = os.path.join(path_image_data, fov)
         paths_images = glob.glob(os.path.join(path, "*.tiff"))
         results = []
         channels = []
@@ -41,27 +93,43 @@ def pixie_example() -> SpatialData:
         sdata = sp.im._add_image_layer(
             sdata,
             arr=da.stack(results, axis=0).squeeze(),
-            output_layer=f"raw_image_fov{i}",
+            output_layer=f"{fov}",
             c_coords=channels,
             overwrite=True,
+            # transformation={
+            #     fov: sd.transformations.Identity()
+            # }
         )
 
         sdata = sp.im._add_label_layer(
             sdata,
-            arr=imread.imread(os.path.join(path_segment_data, f"fov{i}_nuclear.tiff")).squeeze(),
-            output_layer=f"label_nuclear_fov{i}",
+            arr=imread.imread(os.path.join(path_segment_data, f"{fov}_nuclear.tiff")).squeeze(),
+            output_layer=f"label_nuclear_{fov}",
             overwrite=True,
+            # transformation={
+            #     fov: sd.transformations.Identity()
+            # }
         )
 
         sdata = sp.im._add_label_layer(
             sdata,
-            arr=imread.imread(os.path.join(path_segment_data, f"fov{i}_whole_cell.tiff")).squeeze(),
-            output_layer=f"label_whole_fov{i}",
+            arr=imread.imread(os.path.join(path_segment_data, f"{fov}_whole_cell.tiff")).squeeze(),
+            output_layer=f"label_whole_{fov}",
             overwrite=True,
+            # transformation={
+            #     fov: sd.transformations.Identity()
+            # }
         )
-
+    if with_cells_output:
+        prefix = "label_whole_"
+        adata = get_adata(path_post_data / "updated_cell_table.csv", label_prefix=prefix)
+        adata = adata[adata.obs["fov"].isin([prefix + fov for fov in fovs])]
+        stable = sd.models.TableModel.parse(
+            adata, region_key="fov", region=[prefix + fov for fov in fovs], instance_key="label"
+        )
+        sdata.tables["table"] = stable
     return sdata
 
 
 if __name__ == "__main__":
-    pixie_example()
+    pixie_example([0])
