fcdad003421ab205ea7826252ecf1b4f340c1368
ArneD <arnedefauw@gmail.com>
2024-09-05 09:05:24 +0200

    SC_24 rasterize shapes layer, fix issue with offset (#51)

--- src/sparrow/image/_shapes_to_labels.py
+++ src/sparrow/image/_shapes_to_labels.py
@@ -19,6 +19,8 @@ def add_labels_layer_from_shapes_layer(
     """
     Given a shapes layer in a SpatialData object, corresponding masks are created, and added as a labels layer to the SpatialData object.
 
+    The index of the shapes layer will be used as the label in the resulting labels layer (`output_layer`).
+
     Parameters
     ----------
     sdata
@@ -42,11 +44,26 @@ def add_labels_layer_from_shapes_layer(
     ------
     ValueError
         If the provided `shapes_layer` contains 3D polygons.
+    ValueError
+        If the provided `shapes_layer` contains Points.
+    ValueError
+        If 0 is in the index of the `shapes_layer`. As 0 is used as background in the `output_layer`.
     """
     # only 2D polygons are suported.
     has_z = sdata.shapes[shapes_layer]["geometry"].apply(lambda geom: geom.has_z)
     if any(has_z):
-        raise ValueError("Shapes layers contains 3D polygons. " "This is currently not supported.")
+        raise ValueError("Shapes layer contains 3D polygons. " "This is currently not supported.")
+
+    if any(sdata.shapes[shapes_layer].geometry.type == "Point"):
+        raise ValueError(
+            "Shapes layer contains Points. This is currently not supported. Please consider converting the Points to Polygons first using e.g. '.buffer( your_radius, cap_style=your_cap_style )"
+        )
+
+    if 0 in sdata[shapes_layer].index.astype(int):
+        raise ValueError(
+            "0 is in the index of the shapes layer. This is not allowed, because the label 0 is reserved for background. "
+            "Either remove the item from the shapes layer or increase indices of shapes with 1."
+        )
 
     x_min, y_min, x_max, y_max = sdata[shapes_layer].geometry.total_bounds
 
@@ -55,6 +72,17 @@ def add_labels_layer_from_shapes_layer(
     ), f"The maximum of the bounding box of the shapes layer {shapes_layer} is negative. This is not allowed."
     index = sdata[shapes_layer].index.values.astype(int)
 
+    if y_min > 0:
+        y_shape = int((y_max - y_min) + y_min)
+    else:
+        y_shape = int(y_max)
+    if x_min > 0:
+        x_shape = int((x_max - x_min) + x_min)
+    else:
+        x_shape = int(x_max)
+
+    out_shape = [y_shape, x_shape]
+
     _dtype = _get_dtype(index.max())
 
     masks = rasterize(
@@ -62,7 +90,7 @@ def add_labels_layer_from_shapes_layer(
             sdata[shapes_layer].geometry,
             index,
         ),
-        out_shape=[int(y_max - y_min), int(x_max - x_min)],
+        out_shape=out_shape,
         dtype=_dtype,
         fill=0,
     )
