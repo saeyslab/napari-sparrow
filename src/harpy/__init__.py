7a554b0c04b12499d34704f615a03b0b4213308a
Benjamin Rombaut <benjamin.rombaut@ugent.be>
2025-01-23 14:44:24 +0100

    Remove setup.cfg (#87)
    * add contents of setup.cfg to pyproject.toml
    
    * add lockfiles
    
    * remove setup.cfg and MANIFEST
    
    * remove relative import
    
    * revert hatchling backend

--- src/harpy/__init__.py
+++ src/harpy/__init__.py
@@ -11,13 +11,13 @@ try:
 except ImportError:
     pass
 
-from . import (  # noqa: E402
+from harpy import (  # noqa: E402
     datasets,
     io,
     utils,
 )
-from . import image as im  # noqa: E402
-from . import plot as pl  # noqa: E402
-from . import points as pt  # noqa: E402
-from . import shape as sh  # noqa: E402
-from . import table as tb  # noqa: E402
+from harpy import image as im  # noqa: E402
+from harpy import plot as pl  # noqa: E402
+from harpy import points as pt  # noqa: E402
+from harpy import shape as sh  # noqa: E402
+from harpy import table as tb  # noqa: E402
