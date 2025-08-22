f09f644f4e939885ff48c1a269eeed8153139fbf
Lotte Pollaris <lotte.pollaris@ugent.be>
2025-08-22 13:23:40 +0200

    restore mistake of import

--- src/sparrow/plot/_transcripts.py
+++ src/sparrow/plot/_transcripts.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-from arneg.benchmark.general_imaging.util import plot
 import numpy as np
 import pandas as pd
 import seaborn as sns
