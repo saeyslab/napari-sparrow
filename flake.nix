b7d20fb3e8312cab3fadc71f8aefbaf4b74903b2
Arne Gevaert <arne.gevaert@ugent.be>
2025-08-25 16:46:19 +0200

    flake support both uv and micromamba

--- flake.nix
+++ flake.nix
@@ -8,32 +8,44 @@
       system = "x86_64-linux";
       envname = "napari-sparrow";
       pkgs = nixpkgs.legacyPackages.${system};
-      fhs = pkgs.buildFHSEnv {
-        name = "fhs";
+
+      uv-fhs = pkgs.buildFHSEnv {
+        name = "uv-fhs";
+        targetPkgs = pkgs: [
+          pkgs.uv
+          pkgs.libz
+          pkgs.expat
+        ];
+        runscript = "zsh";
+        profile = ''
+          uv venv --python 3.10
+          uv pip install -e .[docs]
+          source .venv/bin/activate
+          export name=uv-fhs
+        '';
+      };
+
+      mm-fhs = pkgs.buildFHSEnv {
+        name = "mm-fhs";
         targetPkgs = pkgs: [
           pkgs.micromamba
-          pkgs.libz
           pkgs.expat
         ];
         runScript = "zsh";
         profile = ''
-          export name=fhs
-
           export MAMBA_ROOT_PREFIX=./.mamba
-
           eval "$(micromamba shell hook -s zsh)"
-
           micromamba create -f environment.yml -n ${envname}
-
           micromamba activate ${envname}
-
-          pip install -e .[plugin,testing,tiling,cli,docs]
-
-          export name=fhs
+          pip install -e .[docs]
+          export name=mm-fhs
         '';
       };
     in
       {
-        devShells.${system}.default = fhs.env;
+        devShells.${system} = {
+          default = mm-fhs.env;
+          uv = uv-fhs.env;
+        };
       };
 }
