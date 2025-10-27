65fc5445d4c1808f9d71f738a30d96e19bf694ff
Arne Gevaert <arne.gevaert@ugent.be>
2025-10-27 07:48:22 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      envname = "napari-sparrow";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      uv-fhs = pkgs.buildFHSEnv {
+        name = "uv-fhs";
+        targetPkgs = pkgs: [
+          pkgs.uv
+          pkgs.libz
+          pkgs.expat
+        ];
+        runscript = "zsh";
+        profile = ''
+          uv venv --python 3.10
+          uv pip install -e .[docs]
+          source .venv/bin/activate
+          export name=uv-fhs
+        '';
+      };
+
+      mm-fhs = pkgs.buildFHSEnv {
+        name = "mm-fhs";
+        targetPkgs = pkgs: [
+          pkgs.micromamba
+          pkgs.expat
+        ];
+        runScript = "zsh";
+        profile = ''
+          export MAMBA_ROOT_PREFIX=./.mamba
+          eval "$(micromamba shell hook -s zsh)"
+          micromamba create -f environment.yml -n ${envname}
+          micromamba activate ${envname}
+          pip install -e .[docs]
+          export name=mm-fhs
+        '';
+      };
+    in
+      {
+        devShells.${system} = {
+          default = mm-fhs.env;
+          uv = uv-fhs.env;
+        };
+      };
+}
