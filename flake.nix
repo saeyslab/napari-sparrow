f8c06dd4bbcbd6399c420a7573cf9e4ac6cdc79d
Arne Gevaert <arne.gevaert@ugent.be>
2025-10-27 11:23:51 +0100

    flake issues

--- flake.nix
+++ flake.nix
@@ -15,11 +15,15 @@
           pkgs.uv
           pkgs.libz
           pkgs.expat
+          pkgs.libGL
+          pkgs.glibc
+          pkgs.glib
         ];
         runscript = "zsh";
         profile = ''
           uv venv --python 3.10
-          uv pip install -e .[docs]
+          uv pip install -e .[docs,testing]
+          uv pip install jupyter
           source .venv/bin/activate
           export name=uv-fhs
         '';
@@ -35,9 +39,9 @@
         profile = ''
           export MAMBA_ROOT_PREFIX=./.mamba
           eval "$(micromamba shell hook -s zsh)"
-          micromamba create -f environment.yml -n ${envname}
+          # micromamba create -f environment.yml -n ${envname}
           micromamba activate ${envname}
-          pip install -e .[docs]
+          pip install -e .[docs,testing]
           export name=mm-fhs
         '';
       };
