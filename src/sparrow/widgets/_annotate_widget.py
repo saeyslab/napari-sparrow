53653fd7be51fbc804b30d097d3022584753cf07
xwieme <xander.wieme@ugent.be>
2025-12-12 14:31:44 +0100

    fix(plugin): correct typo

--- src/sparrow/widgets/_annotate_widget.py
+++ src/sparrow/widgets/_annotate_widget.py
@@ -14,14 +14,14 @@ from napari.utils.notifications import show_info
 from spatialdata import SpatialData, read_zarr
 
 import sparrow.utils as utils
-from sparrow.pipeline import sparrowPipeline
+from sparrow.pipeline import SparrowPipeline
 
 log = utils.get_pylogger(__name__)
 
 
 def annotateImage(
     sdata: SpatialData,
-    pipeline: sparrowPipeline,
+    pipeline: SparrowPipeline,
 ) -> SpatialData:
     """Function representing the annotation step, this calls all the needed functions to annotate the cells with the celltype."""
     sdata = pipeline.annotate(sdata)
@@ -34,7 +34,9 @@ def annotateImage(
 
 
 @thread_worker(progress=True)
-def _annotation_worker(sdata: SpatialData, method: Callable, fn_kwargs: dict[str, Any]) -> SpatialData:
+def _annotation_worker(
+    sdata: SpatialData, method: Callable, fn_kwargs: dict[str, Any]
+) -> SpatialData:
     """Annotate data with marker genes in a thread worker"""
     return method(sdata, **fn_kwargs)
 
@@ -47,7 +49,9 @@ def annotate_widget(
     viewer: napari.Viewer,
     markers_file: pathlib.Path = pathlib.Path(""),
     delimiter: str = ",",
-    del_celltypes: list[str] = [],  # noqa: B006 # noqa: B006 # magicgui does not accept None
+    del_celltypes: list[
+        str
+    ] = [],  # noqa: B006 # noqa: B006 # magicgui does not accept None
 ):
     """Function represents the annotation widget and is called by the wizard to create the widget."""
     # Check if a file was passed
@@ -68,7 +72,9 @@ def annotate_widget(
     try:
         pipeline = allocation_layer.metadata["pipeline"]
     except KeyError as err:
-        raise RuntimeError("Please run allocation step before running annotation step.") from err
+        raise RuntimeError(
+            "Please run allocation step before running annotation step."
+        ) from err
 
     # need to load it back from zarr store, because otherwise not able to overwrite it
     sdata = read_zarr(pipeline.cfg.paths.sdata)
@@ -85,12 +91,14 @@ def annotate_widget(
 
     def add_metadata(
         sdata: SpatialData,
-        pipeline: sparrowPipeline,
+        pipeline: SparrowPipeline,
         layer_name: str,
     ):
         """Add the metadata to the previous layer, this way it becomes available in the next steps."""
         if layer_name not in viewer.layers:
-            log.info(f"Layer '{layer_name}' does not exist. Please run allocation step before running annotation step.")
+            log.info(
+                f"Layer '{layer_name}' does not exist. Please run allocation step before running annotation step."
+            )
             raise KeyError(f"Layer '{layer_name}' does not exist")
 
         # Store data in previous layer
@@ -102,7 +110,9 @@ def annotate_widget(
 
         utils._export_config(
             pipeline.cfg.annotate,
-            os.path.join(pipeline.cfg.paths.output_dir, "configs", "annotate", "plugin.yaml"),
+            os.path.join(
+                pipeline.cfg.paths.output_dir, "configs", "annotate", "plugin.yaml"
+            ),
         )
 
         log.info("Annotation metadata added")
